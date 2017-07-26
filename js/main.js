08d8591e020fc56da887de92616319f951360965
shadowdreamer <uzumaki@yeah.net>
2017-07-26 18:09:24 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 function changeFace(){
     var element=document.getElementById("face")
     if (element.src.match("face2")){
@@ -18,22 +16,6 @@ function multi1(){
     var N2=document.getElementById("N2").value;
     x.innerHTML= multi(N1,N2);
 }  
-
-/*Js获取当前日期时间及其它操作
-var myDate = new Date();
-myDate.getYear();        //获取当前年份(2位)
-myDate.getFullYear();    //获取完整的年份(4位,1970-????)
-myDate.getMonth();       //获取当前月份(0-11,0代表1月)
-myDate.getDate();        //获取当前日(1-31)
-myDate.getDay();         //获取当前星期X(0-6,0代表星期天)
-myDate.getTime();        //获取当前时间(从1970.1.1开始的毫秒数)
-myDate.getHours();       //获取当前小时数(0-23)
-myDate.getMinutes();     //获取当前分钟数(0-59)
-myDate.getSeconds();     //获取当前秒数(0-59)
-myDate.getMilliseconds();    //获取当前毫秒数(0-999)
-myDate.toLocaleDateString();     //获取当前日期
-var mytime=myDate.toLocaleTimeString();     //获取当前时间
-myDate.toLocaleString( );        //获取日期与时间*/
 var currentDate = {
     date : new Date(),
     year : function(){return this.date.getFullYear()},
--- js/myjq.js
+++ js/myjq.js
@@ -3,4 +3,5 @@ $(document).ready(function(){
         $("button#dateB").fadeToggle(300)
     });
     $( "div.tool" ).draggable();
+    $( "div.ico" ).draggable();
 });
